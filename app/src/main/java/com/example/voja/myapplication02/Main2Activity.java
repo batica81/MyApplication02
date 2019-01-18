fa919f08f0df6faac496b563f8740550fad12ef9
Vojislav Ristivojevic <batica@gmail.com>
2019-01-18 17:04:00 +0100

    trash

---
+++ app/src/main/java/com/example/voja/myapplication02/Main2Activity.java
@@ -0,0 +1,13 @@
+package com.example.voja.myapplication02;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
--- app/src/main/java/com/example/voja/myapplication02/MainActivity.java
+++ app/src/main/java/com/example/voja/myapplication02/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.voja.myapplication02;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        WebView myWebView = new WebView(activityContext);
+        setContentView(myWebView);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
+//        WebView myWebView = new WebView(activityContext);
+        setContentView(myWebView);
+
+        myWebView.loadUrl("http://www.example.com");
+
     }
 }
